f1ec64db05de67b45a26d01b68038087d3b08f25
Somashekarreddy Thallapureddy <thallapureddysomashakerreddy5@gmail.com>
2026-01-25 22:03:42 +0530

    Implement function to reverse an array of strings

--- ReverseString (Day-6).js
+++ ReverseString (Day-6).js
@@ -1,1 +1,18 @@
 
+function reverseStringarray(arry) {
+  
+  let length = arry.length
+  let halflength = Math.floor(length/2)
+  for(let i = 0; i< halflength;i++){
+    let temp = arry[i];
+    arry[i] = arry[length-i-1];
+    arry[length-i-1] = temp;
+    
+    
+  }
+  
+  return arry
+}
+let arry = ["a","b","c","d"]
+let re = reverseStringarray(arry)
+console.log(re)
